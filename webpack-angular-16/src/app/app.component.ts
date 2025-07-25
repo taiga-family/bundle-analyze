e01470dfcfee44dddb59b555aacbfd247a306e0e
splincode <omaxphp@yandex.ru>
2025-07-25 16:01:07 +0300

    chore: add taiga-ui

--- webpack-angular-16/src/app/app.component.ts
+++ webpack-angular-16/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {TuiRoot} from "@taiga-ui/core";
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<tui-root>{{title}}</tui-root>',
+  imports: [
+    TuiRoot
+  ]
 })
 export class AppComponent {
   title = 'webpack-angular-16';
--- webpack-angular-16/src/main.ts
+++ webpack-angular-16/src/main.ts
@@ -1,7 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {provideEventPlugins} from '@taiga-ui/event-plugins';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideEventPlugins()]
+}).catch(error => console.error(error));
